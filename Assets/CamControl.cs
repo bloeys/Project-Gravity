5e8a1b7cef6e05bf7a31f61d659ed290a9b2085f
bloeys <bloeys@outlook.com>
2017-03-19 03:39:41 +0400

    Initial Commit

---
+++ Assets/CamControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CamControl : MonoBehaviour
+{
+    public Transform target;
+    public float zoomSpd = 20;
+    Camera cam;
+    int index = 0;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            index++;
+            if (index == GravityController.bodies.Count)
+                index = 0;
+
+            target = GravityController.bodies[index].transform;
+        }
+
+        else if (Input.GetMouseButtonDown(1))
+        {
+            index--;
+            if (index < 0)
+                index = GravityController.bodies.Count - 1;
+
+            target = GravityController.bodies[index].transform;
+        }
+
+        transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+
+        if (Input.mouseScrollDelta.y < 0)
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize + zoomSpd, 400, 10000);
+        else if (Input.mouseScrollDelta.y > 0)
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - zoomSpd, 400, 10000);
+    }
+}
---
+++ Assets/GravityController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+    public static List<Body> bodies { get; private set; }
+    public float gravityConstant = 10;
+    public float timeScale = 1;
+
+    void Update()
+    {
+        Time.timeScale = timeScale;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        for (int i = 0; i < bodies.Count; i++)
+        {
+            for (int j = i + 1; j < bodies.Count; j++)
+            {
+                Vector3 force = bodies[j].transform.position - bodies[i].transform.position;
+                float dist = force.magnitude;
+                force.Normalize();
+                force *= gravityConstant * bodies[i].rb.mass * bodies[j].rb.mass;
+                force /= dist * dist;
+
+                bodies[i].rb.AddForce(force, ForceMode.Force);
+                bodies[j].rb.AddForce(-force, ForceMode.Force);
+            }
+        }
+    }
+
+    public static void AddToBodies(Body b)
+    {
+        if (bodies == null)
+            bodies = new List<Body>();
+        bodies.Add(b);
+    }
+
+    void OnDrawGizmos()
+    {
+        if (bodies == null) return;
+
+        Gizmos.color = Color.green;
+        for (int i = 0; i < bodies.Count; i++)
+            if (bodies != null && bodies[i] != null)
+                Gizmos.DrawLine(bodies[i].transform.position, bodies[i].transform.position + bodies[i].rb.velocity);
+    }
+}
